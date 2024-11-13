4689e6a702ecaa2657c1a89dd1afaf42c6ff2278
hyeonsukk3612 <hyeonsukk3612@gmail.com>
2024-11-13 13:44:31 +0900

    main.cpp

--- dxl1/main.cpp
+++ dxl1/main.cpp
@@ -1,69 +1,36 @@
-#include "opencv2/opencv.hpp"
 #include <iostream>
-using namespace cv;
+#include <unistd.h>
+#include <sys/time.h>
+#include <signal.h>
+#include "dxl.hpp"
 using namespace std;
-
-int main()
+bool ctrl_c_pressed = false;
+void ctrlc(int){ ctrl_c_pressed = true; }
+int main(void)
 {
-    string src = "nvarguscamerasrc sensor-id=0 ! \
-        video/x-raw(memory:NVMM), width=(int)640, height=(int)360, \
-    format=(string)NV12, framerate=(fraction)30/1 ! \
-        nvvidconv flip-method=0 ! video/x-raw, \
-        width=(int)640, height=(int)360, format=(string)BGRx ! \
-        videoconvert ! video/x-raw, format=(string)BGR ! appsink";
-    // NVIDIA Jetson 카메라에서 영상을 캡처
+    Dxl mx;
+    struct timeval start,end1;
+    int leftspeed,rightspeed;
+    double time1;
 
-    VideoCapture source(src, CAP_GSTREAMER);
-    if (!source.isOpened()){ cout << "Camera error" << endl; return -1; }
-    // 카메라 소스 열기 및 오류 확인
+    signal(SIGINT, ctrlc); 				//시그널 핸들러 지정
+    if(!mx.open()) { cout << "dynamixel open error"<<endl; return -1; } //장치열기
+    while(true)
+    {
+        gettimeofday(&start,NULL); 
 
-    string dst1 = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! \
-        nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! \
-        h264parse ! rtph264pay pt=96 ! \
-        udpsink host=203.234.58.168 port=8001 sync=false";
-    // 원본 영상
-
-    VideoWriter writer1(dst1,0, (double)30,Size(640,360),true);
-    if(!writer1.isOpened()) {cerr<<"Writer open failed!"<<endl; return -1;}
-    // 원본 영상 스트리밍
-
-    string dst2 = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! \
-        nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! \
-        h264parse ! rtph264pay pt=96 ! \
-        udpsink host=203.234.58.168 port=8002 sync=false";
-    // 그레이스케일 영상
-
-    VideoWriter writer2(dst2,0, (double)30,Size(640,360),false);
-    if(!writer2.isOpened()) {cerr<<"Writer open failed!"<<endl; return -1;}
-    // 그레이스케일 영상 스트리밍
-
-    string dst3 = "appsrc ! videoconvert ! video/x-raw, format=BGRx ! \
-        nvvidconv ! nvv4l2h264enc insert-sps-pps=true ! \
-        h264parse ! rtph264pay pt=96 ! \
-        udpsink host=203.234.58.168 port=8003 sync=false";
-    // 이진화 영상
-
-    VideoWriter writer3(dst3,0, (double)30,Size(640,360),false);
-    if(!writer3.isOpened()) {cerr<<"Writer open failed!"<<endl; return -1;}
-    // 이진화 영상 스트리밍
-
-    Mat frame, gray, thread;
-    TickMeter timer;
-    
-    while (true) {
-        timer.start();
-        source >> frame;  // 카메라에서 프레임 읽기
-        if (frame.empty()){ cerr << "frame empty!" << endl; break; }
-        cvtColor(frame, gray, COLOR_BGR2GRAY);  // 그레이스케일 변환
-        threshold(gray, thread, 128, 255, THRESH_BINARY);  // 이진화 처리
-        writer1 << frame;  // 원본 영상 스트리밍
-        writer2 << gray;   // 그레이스케일 영상 스트리밍
-        writer3 << thread; // 이진화 영상 스트리밍
-        waitKey(30);  // 30ms 대기
-        timer.stop();
-		cout<<"time : "<<timer.getTimeMilli()<<"ms"<<endl;
-		timer.reset();
+        cout << "left speed:";
+        cin >> leftspeed;
+        cout << "right speed:";
+        cin >> rightspeed;
+        mx.setVelocity(leftspeed, rightspeed); //속도명령 전송
+        usleep(20*1000);                //20msec sleep      
+        if (ctrl_c_pressed) break;		//Ctrl+c 누르면 탈출
+        
+        gettimeofday(&end1,NULL);
+        time1=end1.tv_sec-start.tv_sec +(end1.tv_usec-start.tv_usec)/1000000.0;
+        cout << "time:" << time1 << "sec"<<endl; //실행시간 출력
     }
+    mx.close(); 					//장치닫기
     return 0;
 }
-
